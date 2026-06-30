330ef9db834d2c22dbc18a90d5fb62b56950c24f
Esrafil <esrafil.s.r.man418@gmail.com>
2026-06-30 13:54:43 +0330

    feat: Create Verify page

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import PlaceOrder from "./pages/placeOrder/PlaceOrder";
 import Footer from "./components/footer/Footer";
 import { useState } from "react";
 import LoginPopup from "./components/login-popup/LoginPopup";
+import Verify from "./pages/verify/Verify";
 
 export default function App() {
 	const [showLogin, setShowLogin] = useState(false);
@@ -18,6 +19,7 @@ export default function App() {
 					<Route path="/" element={<Home />} />
 					<Route path="/cart" element={<Cart />} />
 					<Route path="/order" element={<PlaceOrder />} />
+					<Route path="/verify" element={<Verify />} />
 				</Routes>
 			</div>
 			<Footer />
---
+++ src/pages/verify/Verify.tsx
@@ -0,0 +1,30 @@
+import { useContext, useEffect } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { StoreContext } from "../../context/storeContext";
+import axios from "axios";
+
+export default function Verify() {
+	const [searchParams, setSearchParams] = useSearchParams();
+	const success = searchParams.get("success");
+	const orderId = searchParams.get("orderId");
+	const { URL } = useContext(StoreContext);
+    const navigate = useNavigate()
+    
+    const verifyPayment = async => {
+        const response = await axios.post(URL+"/api/order/verify", {success, orderId})
+        if (response.data.success) {
+      navigate("/myorders")
+        }
+        else {navigate("/")}
+    }
+
+    useEffect(() => {
+        verifyPayment()
+    }, [])
+
+	return (
+		<div className="min-h-[60vh] grid place-items-center">
+			<div className="w-12 h-12 border-4 border-turquoise border-t-transparent rounded-full animate-spin"></div>
+		</div>
+	);
+}
